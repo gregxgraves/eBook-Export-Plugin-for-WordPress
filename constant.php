7c1a2d3fbe3a8ce0d54bed05b99f23d31d9328be
Christopher Clarke <chrisclarke@sorrowfulunfounded.com>
2010-01-31 12:30:25 +1100

    Began moving license information out of the ebook.class.php into a file called constant.php. Not sure about that name but values in that file should not need to be changed much.

---
+++ constant.php
@@ -0,0 +1,9 @@
+<?php
+
+$licenses = array(
+    'default' => array(_('Standard Copyright'), _('%s Copyright %i %s. All Rights Reserved.')),
+    'public-domain' => array(_('Public Domain'), _('This eBook has been released into the public domain.')),
+    'cc-by-30' => array(_('Creative Commons Attribution 3.0'), _('%s Copyright %i %s. Some Rights Reserved.'))
+);
+
+?>
