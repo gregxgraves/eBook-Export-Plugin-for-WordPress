9678bc9f04f0569f37b0103e30602c5f571bb915
Christopher Clarke <chrisclarke@sorrowfulunfounded.com>
2010-01-31 12:27:51 +1100

    Renamed eBook_Creator.php to simply ebook.class.php.

--- ebook.class.php
+++ ebook.class.php
@@ -5,51 +5,13 @@ class eBook_Creator {
     public $meta = array();
     public $chapter_category = 0;
 
-    function set_title($title)
+    function set_book_info($book = array())
     {
-        $this->meta['title'] = $title;
-    }
-    
-    function set_isbn($isbn)
-    {
-        $this->meta['isbn'] = $isbn;
-    }
-    
-    function set_isbn13($isbn)
-    {
-        $this->meta['isbn13'] = $isbn;
-    }
-    
-    function set_author($author)
-    {
-        $this->meta['author'] = $author;
-    }
-    
-    function set_license($license)
-    {
-        $this->meta['license'] = $license;
-    }
-    
-    function set($key, $data)
-    {
-        $this->meta[$key] = $data;
+        $this->meta = $book;
     }
     
     function rights()
     {
-/*
-<option value="1">Standard Copyright</option>
-<option value="2">Public Domain</option>
-<optgroup label="Creative Commons">
-    <option value="3">Attribution</option>
-    <option value="4">Attribution-ShareAlike</option>
-    <option value="5">Attribution-NonCommercial</option>
-    <option value="6">Attribution-NonCommercial-ShareAlike</option>
-    <option value="7">Attribution-NoDerivs</option>
-    <option value="8">Attribution-NonCommercial-NoDerivs</option>
-</optgroup>
-<option value="9">GNU Free Documentation License</option>
-*/
 
         $right = $this->meta['license'];
 
@@ -83,11 +45,11 @@ class eBook_Creator {
             break;
             case 8:
                 $text = $this->meta['title'].' Copyright '.date('Y').' '.$this->meta['author'].'. Some Rights Reserved.';
-                $text .= "\n".'This EPUB eBook is released under a Creative Commons (BY-NC-ND/3.0) Licence.';
+                $text .= "\n"._('This EPUB eBook is released under a Creative Commons (BY-NC-ND/3.0) Licence.');
             break;
             case 9:
                 $text = $this->meta['title'].' Copyright '.date('Y').' '.$this->meta['author'].'. Some Rights Reserved.';
-                $text .= "\n".'This EPUB eBook is released under the GNU Free Documentation License 3.0 Licence.';
+                $text .= "\n"._('This EPUB eBook is released under the GNU Free Documentation License 3.0 Licence.');
             break;
             default:
                 $text = $this->meta['title'].' Copyright '.date('Y').' '.$this->meta['author'].'. All Rights Reserved.';
