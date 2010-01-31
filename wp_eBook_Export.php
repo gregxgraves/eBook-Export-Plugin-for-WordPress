2a882734302c0f39ca2635dbb3cc60de4ff6a3d4
Christopher Clarke <chrisclarke@sorrowfulunfounded.com>
2010-01-31 12:33:08 +1100

    - Gettext localisation now supported. - Available formats are now dynamicly generated. - Licenses are now no longer hardcoded. - Form now remembers values if an error occured for all items except drop down menus. - Added ISBN validation but not really in the correct place. Will fix.

--- wp_eBook_Export.php
+++ wp_eBook_Export.php
@@ -1,4 +1,12 @@
 <?php
+/*
+Plugin Name: WP eBook Export
+Plugin URI: http://sorrowfulunfounded.com/wp-ebook-export
+Description: Exports a category as an eBook (currently only ePub).
+Version: 0.1
+Author: Cbristopher Clarke
+Author URI: http://sorrowfulunfounded.com
+*/
 /*  Copyright 2010  Christopher Clarke  (email : chrisclarke@sorrowfulunfounded.com)
 
     This program is free software; you can redistribute it and/or modify
@@ -14,84 +22,88 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-/*
-Plugin Name: WP eBook Export
-Plugin URI: http://sorrowfulunfounded.com/wp-ebook-export
-Description: Exports a category as an eBook (currently only ePub).
-Version: 0.1
-Author: Cbristopher Clarke
-Author URI: http://sorrowfulunfounded.com
-*/
+
+define('WP_EBOOK_CURRENT_PATH', str_replace('wp_ebook_export.php', '', __FILE__));
 
 function wp_eBook_Export_add_menu_item()
 {
-    add_management_page( 'Create eBook', 'eBook Creator', 5, __FILE__, 'wp_eBook_Export_options_page' );
+    add_management_page(_('Create e-book'), _('E-book Manager'), 5, __FILE__, 'wp_eBook_Export_options_page' );
 }
 
 function wp_eBook_Export_options_page()
 {
-    $error = true;
-    
-    if (isset($_POST['create']))
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
     {
-        $error = false;
+        $errors = array();
         if (isset($_POST['book_title']) == false)
         {
-            $error = true;
-            echo '<p><strong>ERROR:</strong> You must enter a book title.</p>';
+            $errors[] = _('You must enter a book title.');
         }
         if (isset($_POST['book_author']) == false)
         {
-            $error = true;
-            echo '<p><strong>ERROR:</strong> You must enter a book author.</p>';
+            $errors[] = _('You must enter a book author.');
         }
         if (isset($_POST['category']) == false)
         {
-            $error = true;
-            echo '<p><strong>ERROR:</strong> You must specify the category containing your chapters.</p>';
+            $errors[] = _('You must specify the category containing your chapters.');
         }
     }
 
-    echo '<h2>Create eBook</h2>';
+    echo '<h2>',_('Create e-book'),'</h2>';
 
-    if ($error == true)
+    if (count($errors) >= 1)
     {
+        echo '<div class="error"><strong>',_('We were unable to create your ebook because the following errors occured:'),'</strong>'
+            ,'<ul>'
+            ,'<li>',implode('</li><li>', $errors),'</li>'
+            ,'</ul></div>';
+    }
+    
+    if (count($errors) >= 1 || $_SERVER['REQUEST_METHOD'] != 'POST')
+    {
+
         wp_ebook_export_admin_form();
+
     } else {
     
-        require_once 'eBook_Creator.php';
-        require_once 'ePub.format.php';
+        $book_info = array();
         
-        $eBook = new ePub;
-        $eBook->set_title($_POST['book_title']);
-        $eBook->set_author($_POST['book_author']);
-        $eBook->set_license($_POST['license']);
-        $eBook->chapter_category = intval($_POST['category']);
-        if (isset($_POST['muse_listing']) && strlen($_POST['muse_listing']) > 10)
+        if (isset($_POST['muses_success_listing']) && strlen($_POST['muses_success_listing']) > 3)
         {
-            $eBook->set('muses-success', $_POST['muse_listing']);
+            $book_info['muses_success_listing'] = $_POST['muses_success_listing'];
         }
-        if (isset($_POST['guide_listing']) && strlen($_POST['guide_listing']) > 10)
+        if (isset($_POST['wfg_listing']) && strlen($_POST['wfg_listing']) > 10)
         {
-            $eBook->set('webfictionguide', $_POST['guide_listing']);
+            $book_info['wfg_listing'] = $_POST['wfg_listing'];
         }
-        if (isset($_POST['webficdic_listing']) && strlen($_POST['webficdic_listing']) > 10)
+        if (isset($_POST['tmwfd_listing']) && strlen($_POST['tmwfd_listing']) > 10)
         {
-            $eBook->set('web-fic-directory', $_POST['webficdic_listing']);
+            $book_info['tmwfd_listing'] = $_POST['tmwfd_listing'];
         }
-        if (isset($_POST['isbn10']) && strlen($_POST['isbn10']) > 10)
-        {
-            $eBook->set_isbn($_POST['isbn10']);
-        }
-        if (isset($_POST['isbn13']) && strlen($_POST['isbn13']) > 10)
-        {
-            $eBook->set_isbn13($_POST['isbn13']);
-        }
-        $eBook->add_chapters();
-        $eBook->save_ebook();
         
-        echo '<p>The eBook has been created successfully.</p>';
-        echo '<p>Download Your eBook:</p>';
+        require_once 'ISBN/ISBN.php';
+        $isbn = new ISBN;
+        
+        if (isset($_POST['isbn10']) && $isbn->validateten($_POST['isbn10']) == true)
+        {
+            $book_info['isbn10'] = $_POST['isbn10'];
+        }
+        if (isset($_POST['isbn13']) && $isbn->validatettn($_POST['isbn13']) == true)
+        {
+            $book_info['isbn13'] = $_POST['isbn13'];
+        }
+
+        require_once 'ebook.class.php';
+        require_once 'epub.format.php';
+
+        $ebook = new ePub;
+        $ebook->chapter_category = intval($_POST['category']);
+        $ebook->set_book_info($book_info);
+        $ebook->add_chapters();
+        $ebook->save_ebook();
+        
+        echo '<div id="message" class="updated fade"><p>',_('The eBook has been created successfully.'),'</p></div>';
+        echo '<p>',_('Download Your eBook:'),'</p>';
         echo '<p><a href="'.plugins_url('eBooks/'.sanitize_title($_POST['book_title']).$eBook->file_extension,__FILE__).'">'.plugins_url('eBooks/'.sanitize_title($_POST['book_title']).$eBook->file_extension,__FILE__).'</a></p>';
         
     }
@@ -100,71 +112,96 @@ function wp_eBook_Export_options_page()
 
 function wp_ebook_export_admin_form()
 {
+    global $current_user;
 
-?><form method="post" action="">
-<table class="form-table">
-<tr valign="top">
-<th scope="row"><label for="book_title">Book Title</label></th>
-<td><input name="book_title" type="text" id="book_title" value="" class="regular-text" /></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="book_author">Book Author Name</label></th>
-<td><input name="book_author" type="text" id="book_author" value="" class="regular-text" /></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="category">Chapter Category:</label></th>
-<td><input name="category" type="text" id="category" value="" class="category" maxlength="4" /> <span class="description">Post category containing the chapters of your book, published in chronological order.</span></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="book_author">License</label></th>
-<td><select name="license">
-<option value="1">Standard Copyright</option>
-<option value="2">Public Domain</option>
-<optgroup label="Creative Commons">
-    <option value="3">Attribution</option>
-    <option value="4">Attribution-ShareAlike</option>
-    <option value="5">Attribution-NonCommercial</option>
-    <option value="6">Attribution-NonCommercial-ShareAlike</option>
-    <option value="7">Attribution-NoDerivs</option>
-    <option value="8">Attribution-NonCommercial-NoDerivs</option>
-</optgroup>
-<option value="9">GNU Free Documentation License</option>
-</select></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="pubyear">First Publication Year</label></th>
-<td><input name="pubyear" type="text" id="pubyear" value="" class="year" maxlength="4" /> <span class="description">What was the first year in which you published this book online or in print, whichever came first?</span></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="isbn10">ISBN-10 Number</label></th>
-<td><input name="isbn10" type="text" id="isbn10" value="" class="regular-text" /> <span class="description">Your International Standard Book Number if you have one</span></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="isbn13">ISBN-13 Number</label></th>
-<td><input name="isbn13" type="text" id="isbn13" value="" class="regular-text" /> <span class="description">Same as above.</span></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="format">eBook Format</label></th>
-<td><select name="format" id="forat"><option value="ePub">EPUB (.epub)</option></select> <span class="description">Formats other then ePub will be available in a future update.</span></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="muse_listing"><a href="http://muses-success.info/">Muse's Success</a> Listing URL</label></th>
-<td><input name="muse_listing" type="text" id="muse_listing" value="" class="regular-text" /> <span class="description">URL to this web fiction's page on Muse's Success.</span></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="guide_listing"><a href="http://webfictionguide.com/">Web Fiction Guide</a> Listing URL</label></th>
-<td><input name="guide_listing" type="text" id="guide_listing" value="" class="regular-text" /> <span class="description">URL to this web fiction's page on WebFictionGuide.</span></td>
-</tr>
-<tr valign="top">
-<th scope="row"><label for="webficdic_listing"><a href="http://www.tonyamoore.com/web-fic-directory/">Web Fic Directory</a> Listing URL</label></th>
-<td><input name="webficdic_listing" type="text" id="webficdic_listing" value="" class="regular-text" /> <span class="description">URL to this web fiction's page on Web Fic Directory.</span></td>
-</tr>
-<tr>
-    <td colspan="2"><input type="submit" name="create" value="Create eBook" /></td>
-</tr>
-</table>
-<?php
+    get_currentuserinfo();
 
+    echo '<form method="post" action="">',"\n"
+        ,'<table class="form-table">',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="book_title">',_('Book Title'),' <span class="required">*</span></label></th>',"\n\t"
+        ,'<td><input name="book_title" type="text" id="book_title" value="',(isset($_POST['book_title']) ? $_POST['book_title'] : ''),'" class="regular-text" /></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="book_author">',_('Book Author Name'),' <span class="required">*</span></label></th>',"\n\t"
+        ,'<td><input name="book_author" type="text" id="book_author" value="',(isset($_POST['book_author']) ? $_POST['book_author'] : $current_user->display_name),'" class="regular-text" /></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="category">',_('Chapter Category'),' <span class="required">*</span></label></th>',"\n\t"
+        ,'<td><input name="category" type="text" id="category" value="" class="category" maxlength="4" />'
+        ,'<span class="description">',_('Post category containing the chapters of your book, published in chronological order.'),'</span></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="book_author">',_('License'),' <span class="required">*</span></label></th>',"\n\t"
+        ,'<td><select name="license">',ebook_license_options(),'</select></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="pubyear">',_('First Publication Year'),'</label></th>',"\n\t"
+        ,'<td><input name="pubyear" type="text" id="pubyear" value="',(isset($_POST['pubyear']) ? $_POST['pubyear'] : date('Y')),'" class="year" maxlength="4" />',
+        '<span class="description">',_('What was the first year in which you published this book online or in print, whichever came first?'),'</span></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="isbn10">',_('ISBN-10 Number'),'</label></th>',"\n\t"
+        ,'<td><input name="isbn10" type="text" id="isbn10" value="',(isset($_POST['isbn10']) ? $_POST['isbn10'] : ''),'" class="regular-text" />'
+        ,'<span class="description">',_('10 digit International Standard Book Number.'),'</span></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="isbn13">',_('ISBN-13 Number'),'</label></th>',"\n\t"
+        ,'<td><input name="isbn13" type="text" id="isbn13" value="',(isset($_POST['isbn13']) ? $_POST['isbn13'] : ''),'" class="regular-text" />'
+        ,'<span class="description">',_('13 digit International Standard Book Number.'),'</span></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="format">',_('eBook Format'),'</label></th>',"\n\t"
+        ,'<td><select name="format" id="format">',ebook_format_options(),'</select></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="muses_success_listing">',sprintf(_('%s Listing'),'<a href="http://muses-success.info/">Muse\'s Success</a>'),'</label></th>',"\n\t"
+        ,'<td>http://muses-success.info/browse/view/<input name="muses_success_listing" type="text" id="muses_success_listing" value="',(isset($_POST['isbn13']) ? $_POST['isbn13'] : ''),'" class="medium-text" />'
+        ,'<span class="description">',sprintf(_('URL to %s listing.'),'Muse\'s Success'),'</span></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="wfg_listing">',sprintf(_('%s Listing'), '<a href="http://webfictionguide.com/">Web Fiction Guide</a>'),'</label></th>',"\n\t"
+        ,'<td>http://webfictionguide.com/listings/<input name="wfg_listing" type="text" id="wfg_listing" value="" class="medium-text" />'
+        ,'<span class="description">',sprintf(_('URL to %s listing.'),'Web Fiction Guide'),'</span></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr valign="top">',"\n\t"
+        ,'<th scope="row"><label for="tmwfd_listing">',sprintf(_('%s Listing'), '<a href="http://www.tonyamoore.com/web-fic-directory/">Web Fic Directory</a>'),'</label></th>',"\n\t"
+        ,'<td><input name="tmwfd_listing" type="text" id="tmwfd_listing" value="" class="regular-text" />'
+        ,'<span class="description">',sprintf(_('URL to %s listing.'),'Tonya R Moore\'s Web Fic Directory'),'</span></td>',"\n"
+        ,'</tr>',"\n"
+        ,'<tr>',"\n\t"
+        ,'<td colspan="2"><input type="submit" name="create" value="',_('Create e-book'),'" /></td>',"\n"
+        ,'</tr>',"\n"
+        ,'</table>',"\n"
+        ,'<p><span class="required">*</span> ',_('indicates a required field.'),'</p>';
+}
+
+function ebook_format_options()
+{
+    $options = '';
+    $handle = opendir(WP_EBOOK_CURRENT_PATH);
+    while (false != ($file = readdir($handle)))
+    {
+        if (strpos($file, '.format.php') != false)
+        {
+            $format = str_replace('.format.php', '', $file);
+            $format = explode('.', $format);
+            $options .= '<option value="'.$format[1].'|'.$format[0].'">'.$format[0].' (.'.$format[1].')</option>';
+        }
+    }
+    closedir($handle);
+    return $options;
+}
+
+function ebook_license_options()
+{
+    require_once 'constant.php';
+    $options = '';
+    foreach ($licenses as $license_abbr => $license_detail)
+    {
+        $options .= '<option value="'.$license_abbr.'">'.$license_detail[0].'</option>';
+    }
+    return $options;
 }
     
 add_action('admin_menu', 'wp_eBook_Export_add_menu_item');
